7d8afc8a09b5aa5a54fd2d2f2be8846a85c9b81e
Icecovery <Icecovery@foxmail.com>
2022-10-28 02:30:15 -0400

    Mesh cutter setup

---
+++ FreeIva_Unity/Assets/Scripts/MeshCutter.cs
@@ -0,0 +1,50 @@
+﻿using Parabox.CSG;
+using UnityEngine;
+
+public class MeshCutter : MonoBehaviour
+{
+    public GameObject[] tools;
+
+    void Start()
+    {
+        Transform p = new GameObject("temp").transform;
+        p.parent = transform.parent;
+
+        p.localPosition = transform.localPosition;
+        p.localRotation = transform.localRotation;
+        p.localScale = transform.localScale;
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            transform.GetChild(i).SetParent(p);
+        }
+
+        foreach (GameObject tool in tools)
+        {
+            tool.transform.parent = transform;
+        }
+
+        transform.parent = null;
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+
+		Model model = CSG.SubtractMultiple(gameObject, tools);
+        model.mesh.Optimize();
+		GetComponent<MeshFilter>().sharedMesh = model.mesh;
+
+		transform.parent = p.parent;
+		transform.localPosition = p.localPosition;
+        transform.localRotation = p.localRotation;
+        transform.localScale = p.localScale;
+        for (int i = p.childCount - 1; i >= 0; i--)
+        {
+            p.GetChild(i).SetParent(transform, false);
+        }
+
+		Destroy(p.gameObject);
+        foreach (GameObject tool in tools)
+        {
+			Destroy(tool);
+		}
+	}
+}
