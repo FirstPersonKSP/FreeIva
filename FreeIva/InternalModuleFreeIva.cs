efacdc16799249f986dd40d0a00b4db5172d3fc4
JonnyOThan <jonnyothan@gmail.com>
2022-10-30 11:28:45 -0400

    Fix #16: internal colliders are now in MODEL nodes -also moved space models from Models to Spaces dir

---
+++ FreeIva/InternalModuleFreeIva.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FreeIva
+{
+    internal class InternalModuleFreeIva : InternalModule
+    {
+        [KSPField]
+        public string shellColliderName = string.Empty;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            if (shellColliderName != string.Empty)
+            {
+                var transform = internalProp.FindModelTransform(shellColliderName);
+                if (transform != null)
+                {
+                    foreach (var meshCollider in transform.GetComponentsInChildren<MeshCollider>())
+                    {
+                        meshCollider.convex = false;
+                    }
+                }
+            }
+        }
+    }
+}
