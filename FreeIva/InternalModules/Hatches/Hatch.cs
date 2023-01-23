82ff60ff8b1e2a82b4f23aeec5e08375f10bcc07
JonnyOThan <jonnyothan@gmail.com>
2023-01-23 16:31:25 -0500

    Fix #173: use current position of attachnode for tube scaling

--- FreeIva/InternalModules/Hatches/Hatch.cs
+++ FreeIva/InternalModules/Hatches/Hatch.cs
@@ -301,7 +301,8 @@ namespace FreeIva
 					// note that some internal hatches have tubeExtent set in their config
 					if (tubeExtent == 0 && myAttachNode != null && _connectedHatch != null)
 					{
-						tubeExtent = Vector3.Dot(myAttachNode.originalPosition, myAttachNode.originalOrientation.normalized);
+						// note: using position here instead of originalPosition to accomodate b9ps potentially changing it
+						tubeExtent = Vector3.Dot(myAttachNode.position, myAttachNode.originalOrientation.normalized);
 					}
 
 					if (tubeExtent != 0)
