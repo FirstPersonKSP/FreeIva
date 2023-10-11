238506f9b3d5a25f636f136c66c2e9eaf1e13707
JonnyOThan <jonnyothan@gmail.com>
2023-10-11 12:08:45 -0400

    add HideFlagsUtility

---
+++ FreeIva_Unity/Assets/Scripts/HideFlagsUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class HideFlagsUtility
+{
+	static void SetHideFlagsRecursive(GameObject go)
+	{
+		Debug.Log($"{go.name} - {go.hideFlags}");
+
+		switch (go.hideFlags)
+		{
+			case HideFlags.HideAndDontSave:
+				go.hideFlags = HideFlags.DontSave;
+				break;
+			case HideFlags.HideInHierarchy:
+			case HideFlags.HideInInspector:
+			case HideFlags.NotEditable:
+				go.hideFlags = HideFlags.None;
+				break;
+		}
+
+		foreach (Transform child in go.transform)
+		{
+			SetHideFlagsRecursive(child.gameObject);
+		}
+	}
+
+	[MenuItem("Help/Hide Flags/Show All Objects")]
+	private static void ShowAll()
+	{
+		foreach (GameObject go in Selection.objects)
+		{
+			SetHideFlagsRecursive(go);
+		}
+	}
+}
