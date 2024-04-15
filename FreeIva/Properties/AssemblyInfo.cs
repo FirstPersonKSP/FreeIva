d154e1861d23c748f3b5e1e1e506748a0cb03227
JonnyOThan <jonnyothan@gmail.com>
2024-04-15 14:42:27 -0400

    integrate KSPBuildTools

--- FreeIva/Properties/AssemblyInfo.cs
+++ FreeIva/Properties/AssemblyInfo.cs
@@ -22,16 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("44e7ef91-ec1f-4270-9bbf-e8a002a08689")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:=
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.18.4")]
 [assembly: AssemblyFileVersion("0.2.18.4")]
 
