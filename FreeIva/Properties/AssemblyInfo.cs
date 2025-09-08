bc417c3158144f154595e07c6e4a3f7eb5de2ba4
JonnyOThan <jonnyothan@gmail.com>
2025-09-08 14:08:43 -0400

    update to KSPBT 0.0.4 and sdk-style project

--- FreeIva/Properties/AssemblyInfo.cs
+++ FreeIva/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FreeIva")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FreeIva")]
 [assembly: AssemblyCopyright("Copyright © pizzaoverhead 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,7 +18,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("44e7ef91-ec1f-4270-9bbf-e8a002a08689")]
 
-[assembly: AssemblyVersion("0.2.20.0")]
-[assembly: AssemblyFileVersion("0.2.20.0")]
-
-[assembly: KSPAssembly("FreeIva", 0, 2, 20)]
