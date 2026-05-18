4f64cab4b529cd56df9a6ce993b6d730786534e2
JonnyOThan <jonnyothan@gmail.com>
2026-05-18 17:21:37 -0400

    Add GravityGimbal and InstanceInternal modules (#526)
    it's the age of alliteration!
    
    Fixes #515 and #524

---
+++ FreeIva/InternalModules/GravityGimbal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FreeIva.InternalModules
+{
+	// TODO: provide angle limits for rotation
+
+	internal class GravityGimbal : InternalModule
+	{
+		[KSPField]
+		public Vector3 rotationAxis = new Vector3(1, 0, 0); // in prop space
+
+		[KSPField]
+		public string transformName = string.Empty;
+
+		[KSPField]
+		public float minAccel = 0.05f;
+
+		[KSPField]
+		public float smoothingFactor = 0.2f;
+
+		[SerializeField] Transform m_controlledTransform;
+
+		InternalModuleFreeIva m_freeIvaModule;
+		Vector3 m_rotationAxisInternalSpace;
+		Quaternion m_defaultRotation;
+
+		public override void OnLoad(ConfigNode node)
+		{
+			base.OnLoad(node);
+
+			if (HighLogic.LoadedScene == GameScenes.LOADING)
+			{
+				if (transformName != string.Empty)
+				{
+					m_controlledTransform = TransformUtil.FindPropTransform(internalProp, transformName);
+				}
+				else
+				{
+					m_controlledTransform = internalProp.hasModel ? transform : internalModel.transform;
+				}
+			}
+		}
+
+		protected void Start()
+		{
+			m_freeIvaModule = InternalModuleFreeIva.GetForModel(internalModel);
+			m_rotationAxisInternalSpace = transform.TransformDirection(rotationAxis);
+			m_defaultRotation = transform.rotation;
+		}
+
+		void FixedUpdate()
+		{
+			if (m_controlledTransform != null)
+			{
+				Vector3 subjectiveGravity = FreeIva.GetInternalSubjectiveAcceleration(m_freeIvaModule, m_controlledTransform.position);
+				Quaternion targetRotation;
+
+				if (KerbalIvaController.UseHorizon(subjectiveGravity, m_freeIvaModule.Centrifuge != null))
+				{
+					Vector3 forward = Vector3.Cross(subjectiveGravity, m_rotationAxisInternalSpace);
+					Vector3 up = Vector3.Cross(forward, m_rotationAxisInternalSpace);
+
+					targetRotation = Quaternion.LookRotation(forward, up);
+				}
+				else
+				{
+					targetRotation = m_defaultRotation;
+				}
+
+				m_controlledTransform.rotation = Quaternion.Lerp(m_controlledTransform.rotation, targetRotation, smoothingFactor);
+			}
+		}
+	}
+}
---
+++ FreeIva/InternalModules/InstanceInternal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FreeIva.InternalModules
+{
+	class ConfigNodeHolder : ScriptableObject
+	{
+		public ConfigNode Node;
+	}
+
+	internal class InstanceInternal : InternalModule
+	{
+		[SerializeReference]
+		ConfigNodeHolder m_prefabInternalNode;
+		ConfigNode m_internalNode;
+
+		[KSPField]
+		public string parentTransformName = string.Empty;
+		[SerializeField] Transform m_parentTransform;
+
+		public override void OnLoad(ConfigNode moduleNode)
+		{
+			base.OnLoad(moduleNode);
+
+			var internalNode = moduleNode.GetNode("INTERNAL");
+
+			if (HighLogic.LoadedScene == GameScenes.LOADING)
+			{
+				m_prefabInternalNode = ScriptableObject.CreateInstance<ConfigNodeHolder>();
+				m_prefabInternalNode.Node = internalNode;
+
+				if (parentTransformName != string.Empty)
+				{
+					m_parentTransform = TransformUtil.FindPropTransform(internalProp, parentTransformName);
+				}
+				else
+				{
+					m_parentTransform = internalProp.hasModel ? transform : internalModel.transform;
+				}
+			}
+
+			if (internalNode != null)
+			{
+				m_internalNode = internalNode;
+			}
+		}
+
+		void Start()
+		{
+			m_internalNode = m_internalNode ?? m_prefabInternalNode?.Node;
+			string internalName = m_internalNode?.GetValue("name");
+
+			if (internalName == null || m_parentTransform == null)
+			{
+				return;
+			}
+
+			var internalPrefab = PartLoader.GetInternalPart(internalName);
+			if (internalPrefab != null)
+			{
+				var internalModel = GameObject.Instantiate(internalPrefab);
+				internalModel.transform.SetParent(m_parentTransform, false);
+				internalModel.part = part;
+				internalModel.gameObject.SetActive(true);
+				internalModel.Load(m_internalNode);
+			}
+		}
+	}
+}
